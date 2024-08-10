f9a25e4b80f0f99e327a6c2f9102090731b62a43
ROCabral <rocabral.adm@gmail.com>
2024-08-10 15:58:08 -0300

    BufferedReader e FileReader.

---
+++ FileReader_BufferedReader/src/main/java/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String path = "/home/rocabral/Documentos/in.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
