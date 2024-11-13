a888390bb8f15917bbca080a7ee53ef16607c2ed
ROCabral <rocabral.adm@gmail.com>
2024-11-13 00:20:31 -0300

    experiência com comparator - usando funções anônimas.

---
+++ experiencia_comparator/src/main/java/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        //list.sort(new MyComparator());
+
+        //isto é o mesmo que o que está abaixo Arrow Function
+        /*Comparator<Product> comparator = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };*/
+
+        //função anônima
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        list.forEach(System.out::println);
+    }
+}
